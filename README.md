b2f076e4616d46831a53040837c9569085fd223a
D-Claffey-Code-Institute Claffey <d7x359i6@students.codeinstitute.net>
2024-08-18 01:54:29 +0000

    Add features section to the readme

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
-### Star Trek Quiz
+# Star Trek Quiz
 
 The Star Trek Quiz is an interactive quiz consisting of 10 questions from the Star Trek Universe.
-[Live Site](https://d-claffey-code-institute.github.io/PP2--Star-Trek-Quiz-2/)
+
+Users will attempt to answer as many questions as possible and at the end of the quiz will be evaluated.
+
+You can click [here](https://d-claffey-code-institute.github.io/PP2--Star-Trek-Quiz-2/) to see the live site.
+
+![Responsive Mockup](assets/images/am_i_responsive.png)
+
+## Features
+
+### Exsisting Features
+
+- _Landing Page_
+  - ![Landing Page](assets/images/landing_page.png)
+  - This the page the user arrives at when entering the site, it shows the rules and the start of the quiz.
+
+- _Rules_ 
+  - Informs the user how toprogress and what will happen when they have answered all the questions.
+
+- _Retry_
+  - The user is given an option to retry the quiz once they have finished.
+
+- _Show Answers_
+  - The user is given the option to see the answers to the questions they got wrong once they have finished the quiz.
+
+- _Shuffle Answers_
+  - Everytime the user starts the quiz the answers are shuffled so the user just doesn't remember the order.
+
+### Future Features
+
+- A leaderboard system.
+- A larger variety of questions.
+- Different questions depending on what series of Star Trek they select.
+- A difficulty system to make the quiz more or less challenging.
+
+## Testing
+
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 // Questions and Answers*
 
 const quizData = [{
-        question: "Who was the captain in the first pilot episode of the original Star Trek Series?",
+        question: "Question 1: Who was the captain in the first pilot episode of the original Star Trek Series?",
         options: ["James T. Kirk", "Jean-Luc Picard", "Christopher Pike", "Spock"],
         answer: "Christopher Pike",
     },
     {
-        question: "Who wrote the 'The Theme From Star Trek'?",
+        question: "Question 2: Who wrote the 'The Theme From Star Trek'?",
         options: ["Gene Roddenberry", "John Williams", "Alexander Courage", "William Shatner"],
         answer: "Alexander Courage"
     },
     {
-        question: "What is James T. Kirk's Fathers name?",
+        question: "Question 3: What is James T. Kirk's Fathers name?",
         options: ["George", "Greg", "James", "Christopher"],
         answer: "George"
     },
     {
-        question: "What was Spock's mother's name?",
+        question: "Question 4: What was Spock's mother's name?",
         options: ["Winona", "Katherine", "Amala", "Amanda"],
         answer: "Amanda"
     },
     {
-        question: "What was Captain Janeway's first mission after becoming the captain of the USS Voyager?",
+        question: "Question 5: What was Captain Janeway's first mission after becoming the captain of the USS Voyager?",
         options: ["Fight the Borg", "Capture the Maquis ship", "Explore the Delta Quadrant", "Protect the Ocampa"],
         answer: "Capture the Maquis ship",
     },
     {
-        question: "Whats the name of the ship under Benjamin Sisko's command?",
+        question: "Question 6: Whats the name of the ship under Benjamin Sisko's command?",
         options: ["Enterprise", "Defiant", "Dominion", "Voyager"],
         answer: "Defiant",
     },
     {
-        question: "The theme song for Star Trek Enterprise is a cover of a song originally sung by:",
+        question: "Question 7: The theme song for Star Trek Enterprise is a cover of a song originally sung by:",
         options: ["Sheryl Crow", "Don Henley", "Neil Diamond", "Rod Stewart"],
         answer: "Rod Stewart",
     },
     {
-        question: "Who was the first communications officer on the Enterprise?",
+        question: "Question 8: Who was the first communications officer on the Enterprise?",
         options: ["Hoshi Sato", "Harry Kim", "Tasha Yar", "Nyota Uhara"],
         answer: "Hoshi Sato",
     },
     {
-        question: "Who did not appear in at least three Star Trek TV series?",
+        question: "Question 9: Who did not appear in at least three Star Trek TV series?",
         options: ["Reginald Barclay", "Q", "William Riker", "Borg"],
         answer: "Reginald Barclay",
     },
     {
-        question: "Who provided the computer voice in the Star Trek TV franchise?",
+        question: "Question 10: Who provided the computer voice in the Star Trek TV franchise?",
         options: ["Gene Roddenberry", "Majel Barrett", "Christine Chapel", "William Shatner"],
         answer: "Majel Barrett",
     },
